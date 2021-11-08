da8ffa3723c7b1bd481ba281879e0a0b8198c8ed
goenka811 <rishabhgoenka@gmail.com>
2021-11-08 17:17:12 +0000

    Updated

--- src/main/java/com/example/mongospringapi/MongoSpringApiApplication.java
+++ src/main/java/com/example/mongospringapi/MongoSpringApiApplication.java
@@ -21,36 +21,52 @@ public class MongoSpringApiApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(PolicyRepository repository, MongoTemplate mongoTemplate){
+	CommandLineRunner runner(PolicyRepository repository, MongoTemplate mongoTemplate) {
 		return args -> {
 			Address address = new Address(
-			"Scotland",
-			"Edinburgh",
-			"EH12", "80 Henry Street"
+					"Scotland",
+					"Edinburgh",
+					"EH12", "80 Henry Street"
 			);
 			Policy policy = new Policy(
-				"SanjRish",
-				"123456",
-				"Annual",
-				address,
-				List.of("GBP", "USD", "EUR"),
+					"SanjRish",
+					"1234568",
+					"Annual",
+					address,
+					List.of("GBP", "USD", "EUR"),
 					BigDecimal.TEN,
 					LocalDateTime.now(),
-			LocalDateTime.now());
+					LocalDateTime.now());
 
-			Query query = new Query();
-			query.addCriteria(Criteria.where("policyNumber").is("12345"));
-			List<Policy> policies = mongoTemplate.find(query, Policy.class);
-			if(policies.size() > 1){
-				throw new IllegalStateException( "Error document found with id" + "12345");
-			}
-			if(policies.isEmpty()) {
-				System.out.println("Inserting row:");
+			//usingMongoTemplateAndQuery(repository, mongoTemplate, policy);
+			repository.findPolicyByPolicyNumber("12345678").ifPresentOrElse( p -> {
+				System.out.println(p + " already exists");
+			}, () -> {
+				System.out.println("Inserting policy "+ policy);
 				repository.insert(policy);
-			}else{
-				System.out.println("Problem inserting row:"+policy);
-			}
+			});
 		};
 	}
+	private void usingMongoTemplateAndQuery(PolicyRepository repository, MongoTemplate mongoTemplate, Policy policy){
+	Query query = new Query();
+			query.addCriteria(Criteria.where("policyNumber").
 
+	is("12345678"));
+	List<Policy> policies = mongoTemplate.find(query, Policy.class);
+			if(policies.size()>1)
+
+	{
+		throw new IllegalStateException("Error document found with id" + "12345");
+	}
+			if(policies.isEmpty())
+
+	{
+		System.out.println("Inserting row:");
+		repository.insert(policy);
+	}else
+
+	{
+		System.out.println("Problem inserting row:" + policy);
+	}
+}
 }
--- src/main/java/com/example/mongospringapi/PolicyRepository.java
+++ src/main/java/com/example/mongospringapi/PolicyRepository.java
@@ -2,5 +2,9 @@ package com.example.mongospringapi;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface PolicyRepository extends MongoRepository <Policy,String> {
+    Optional<Policy> findPolicyByPolicyNumber(String number);
 }
