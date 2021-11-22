3df3289f34476ee05f4936a9c697cee182136f92
goenka811 <rishabhgoenka@gmail.com>
2021-11-22 20:04:58 +0000

    Updated

--- src/main/java/com/example/mongospringapi/ClientService.java
+++ src/main/java/com/example/mongospringapi/ClientService.java
@@ -1,6 +1,8 @@
 package com.example.mongospringapi;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +12,14 @@ import java.util.List;
 public class ClientService {
 
     private final ClientRepository clientRepository;
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientService.class);
     public List<Client> getAllClients() {
+        LOGGER.trace("Entering getAllClients");
+        LOGGER.debug("Finding All clients" + "debuglog");
+        if(clientRepository.findAll() == null){
+            LOGGER.error("No Clients found - error");
+        }
+        LOGGER.info("Clients found successfully");
         return clientRepository.findAll();
     }
 }
